5e3bd746b11891eb7ae8935d84574fc299245776
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-08-16 21:42:19 +0300

    Add abstract

---
+++ UdemyRapidApi/ApiConsume/HotelProject.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Abstract
+{
+    public interface IGenericDal <T> where T : class
+    {
+        Task InsertAsync(T t);
+        Task UpdateAsync(T t);
+        Task DeleteAsync(T t);
+        Task<List<T>> GetListAsync();
+        Task<T> GetByIdAsync(int id);
+
+    }
+}
