051c8e9998cdcd9458a4356d1bdfda9857b0754a
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-08-21 16:18:15 +0300

    Personel ekleme işlemi taamam

--- UdemyRapidApi/FrontEnd/HotelProject.WebUI/Models/Staff/AddStaffViewModel.cs
+++ UdemyRapidApi/FrontEnd/HotelProject.WebUI/Models/Staff/AddStaffViewModel.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HotelProject.EntityLayer.Concrete
+﻿namespace HotelProject.WebUI.Models.Staff
 {
-    public class Staff
+    public class AddStaffViewModel
     {
-        public int StaffId { get; set; }
-
         public string Name { get; set; }
 
         public string Title { get; set; }
+
         public string SocialMedia1 { get; set; }
         public string SocialMedia2 { get; set; }
         public string SocialMedia3 { get; set; }
-
     }
 }
