289411760fa2597228f0262b249a8d452a38fe3a
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-08-22 16:11:36 +0300

    Fe tarafında service dtoları oluşturuldu

---
+++ UdemyRapidApi/FrontEnd/HotelProject.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class UpdateServiceDto
+    {
+
+        public int ServiceId { get; set; }
+        [Required(ErrorMessage = "Hizmet ikon linki giriniz")]
+        public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Hizmet Başlığı giriniz")]
+        [StringLength(100, ErrorMessage = "Hizmet başlığı maks 100 karakter olabilir")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Hizmet açıklma metni giriniz")]
+        [StringLength(500, ErrorMessage = "Hizmet açıklama metni maks 500 karakter olabilir")]
+        public string Description { get; set; }
+    }
+}
