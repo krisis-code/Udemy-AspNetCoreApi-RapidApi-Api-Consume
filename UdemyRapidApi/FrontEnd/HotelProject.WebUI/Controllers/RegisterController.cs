34615d3c07086e928972c4d2609940fbb175c903
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-08-24 15:14:54 +0300

    RegisterDto oluşturuldu

---
+++ UdemyRapidApi/FrontEnd/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,21 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RegisterControlle : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterControlle(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
