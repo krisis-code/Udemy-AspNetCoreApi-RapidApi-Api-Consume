4957d4f42fbaba2629abe666ddca612e7b43909c
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-08-25 17:39:19 +0300

    Subscribe consume edildi

--- UdemyRapidApi/FrontEnd/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ UdemyRapidApi/FrontEnd/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -6,6 +6,7 @@ using HotelProject.WebUI.Dtos.RegisterDto;
 using HotelProject.WebUI.Dtos.RoomDto;
 using HotelProject.WebUI.Dtos.ServiceDto;
 using HotelProject.WebUI.Dtos.StaffDto;
+using HotelProject.WebUI.Dtos.SubscribeDto;
 using HotelProject.WebUI.Dtos.TestimonialDto;
 
 namespace HotelProject.WebUI.Mapping
@@ -25,6 +26,7 @@ namespace HotelProject.WebUI.Mapping
             CreateMap<RoomResultDto, Room>().ReverseMap();
             CreateMap<ResultTestimonialDto, Testimonial>().ReverseMap();
             CreateMap<ResultStaffDto, Staff>().ReverseMap();
+            CreateMap<CreateSubscribeDto, Subscribe>().ReverseMap();
 
         }
     }
