d2c29c4c57526715aea508c81260fc18fcea5be8
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-09-02 16:08:59 +0300

    Guest Güncelelme işlemide tamamlandı

--- UdemyRapidApi/FrontEnd/HotelProject.WebUI/Controllers/GuestAdminController.cs
+++ UdemyRapidApi/FrontEnd/HotelProject.WebUI/Controllers/GuestAdminController.cs
@@ -59,29 +59,29 @@ namespace HotelProject.WebUI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> UpdateRoom(int id)
+        public async Task<IActionResult> UpdateGuest(int id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"http://localhost:5290/api/Room/{id}");
+            var responseMessage = await client.GetAsync($"http://localhost:5290/api/Guest/{id}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<RoomUpdateDto>(jsonData);
+                var values = JsonConvert.DeserializeObject<GuestUpdateDto>(jsonData);
                 return View(values);
             }
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> UpdateRoom(RoomUpdateDto roomUpdateDto)
+        public async Task<IActionResult> UpdateGuest(GuestUpdateDto guestUpdateDto)
         {
             if (!ModelState.IsValid)
             {
                 return View();
             }
             var client = _httpClientFactory.CreateClient();
-            var jsonData = JsonConvert.SerializeObject(roomUpdateDto);
+            var jsonData = JsonConvert.SerializeObject(guestUpdateDto);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PutAsync("http://localhost:5290/api/Room/", stringContent);
+            var responseMessage = await client.PutAsync("http://localhost:5290/api/Guest/", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
