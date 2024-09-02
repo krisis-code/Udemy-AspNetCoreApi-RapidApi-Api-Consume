d6d2c5a272bc217fe1c67d5ffe57a9de463d1b88
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-09-02 16:05:41 +0300

    Müşteri ekleme işlemi tamamlandı

---
+++ UdemyRapidApi/FrontEnd/HotelProject.WebUI/Controllers/GuestAdminController.cs
@@ -0,0 +1,92 @@
+﻿using HotelProject.WebUI.Dtos.GuestDto;
+using HotelProject.WebUI.Dtos.RoomDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class GuestAdminController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public GuestAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5290/api/Guest");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<GuestResultDto>>(jsonData);
+                return View(values);
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddGuest()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddGuest(GuestAddDto guestAddDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(guestAddDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5290/api/Guest", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteRoom(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:5290/api/Room/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateRoom(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5290/api/Room/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<RoomUpdateDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateRoom(RoomUpdateDto roomUpdateDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(roomUpdateDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:5290/api/Room/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
