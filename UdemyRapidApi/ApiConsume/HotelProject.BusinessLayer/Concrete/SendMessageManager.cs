fdebb2f418f20e528f4b96fb2ef58cebd39e9d59
Enes Başpınar <Menesbaspinar@hotmail.com>
2024-12-03 21:34:09 +0300

    ISendMessageService interface'i ve SendMessageManager class'ı eklendi: - ISendMessageService, IGenericService<SendMessage> interface'inden türetildi. - SendMessageManager, GenericManager'dan miras alarak ISendMessageDal dependency'si ile oluşturuldu.

---
+++ UdemyRapidApi/ApiConsume/HotelProject.BusinessLayer/Concrete/SendMessageManager.cs
@@ -0,0 +1,20 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class SendMessageManager : GenericManager<SendMessage>, ISendMessageService
+    {
+        private readonly ISendMessageDal _sendMessageDal;
+        public SendMessageManager(ISendMessageDal sendMessageDal) : base(sendMessageDal)
+        {
+            _sendMessageDal = sendMessageDal;
+        }
+    }
+}
